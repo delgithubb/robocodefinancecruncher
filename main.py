d40d1e62347f78ea9109d28cc408739bbccf4096
delgithubb <davoduko25@gmail.com>
2024-05-21 16:21:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import csv
+
+
+with open('data.csv', 'a',newline ='') as f:
+     date = input('Please type the date for the following products: ')
+     counter = int(input('Please type the amount of products you would like to add'))
+     while counter != 0:
+     
+          item = input('Type item name: ')
+          price = (input('Type Price: '))
+          quantity = (input('Type Quantity: '))
+          fw.writerow((item,date, price, quantity))
+
+          counter -=1
+     f.close()
+
+#add the two multiple items
+#delete file and ulpoad to drive
