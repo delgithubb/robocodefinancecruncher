5b40c7f9ea0282f8c4b3dc75cd27b2c9c1a164f3
delgithubb <davoduko25@gmail.com>
2024-05-22 16:15:58 +0100

    graph

--- main.py
+++ main.py
@@ -1,61 +1,40 @@
-from dash import Dash, html, dcc, Input, Output
-from datetime import time
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
+import csv
+
+datacsv = "./data.csv"
+data = dict()
+
 
 app = Dash(__name__)
-df = pd.read_csv('./data.csv')
 
+app.layout = html.Div([
+    html.H4('Simple stock plot with adjustable axis'),
+    html.Button("Switch Axis", n_clicks=0, 
+                id='button'),
+    dcc.Graph(id="graph"),
+])
 
-
-df = df.reindex(index=df.index[::-1])
-date = []
-
-df['date'] = Date
-
-
-
-
-app.layout =html.Div([
-    dcc.Graph(id='graph',
-              figure = {},
-                 ),
-    dcc.RadioItems(
-    [
-        {
-            "label": html.Div(['All'], style={'color': 'Black', 'font-size': 25, 'display' : 'inline-block', 'font-family' : 'Verdana','width' : '10%', 'margin': 'auto', 'padding' : '10px'  }),
-            "value": "all",
-        }
-
-    ], value='simple', inline = False, id = 'radio-button-picker'
-)])
-
+#fix this function so graph works
+def datexsalesy():
+    with open(datacsv) as f:
+        reader=csv.reader(f)
+        for row in reader:
+            csv.writer(data).writerow(row['Date'], row['Sales'])
+        
 
 @app.callback(
-    Output('graph', 'figure'),
-    Input('radio-button-picker', 'value'))
-def update_graph():
-    df=df.copy()
+    Output("graph", "figure"), 
+    Input("button", "n_clicks"))
+def display_graph(n_clicks):
+    datexsalesy()
+    df = pd.read_csv('./data.csv') # replace with your own data source
 
-    fig = px.line(dfr, x = 'date', y = 'sales', labels={
-                     "date": "Date of Purchase",
-                     "sales": "Sales(£)",
-                 },
-                title="Pink Morsel Sales - Soul Foods")
-
-    fig.update_layout(
-        font_family="Verdana",
-        font_color="Black",
-        font_size=15,
-        title_font_family="Arial",
-        title_font_color="black",
-        title_font_size=30
-    )
-
+    x = 'Sales'
+    y = 'Date'
+    fig = px.line(data, x=x, y=y)    
     return fig
 
 
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
+app.run_server(debug=True)
