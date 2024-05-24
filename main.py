137bcf43bc90f6a9f7e4a3b0a69e23fce9e96929
delgithubb <davoduko25@gmail.com>
2024-05-24 15:42:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,31 @@ data = data.to_dict(orient='records')
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H4('Simple stock plot with adjustable axis'),
+    html.Label('RoboCode Component Spending', style={'color':'Black', 'font-size':25,'centre':True, 'font-family' : 'Arial' , 'align':"center"  } ),
     dcc.Graph(id="graph"),
-])
+    dcc.RadioItems([
+                {
+            'label':html.Div(['Item Over Total Spending'], style={'color': 'Black', 'font-size': 15, 'display' : 'inline-block', 'font-family' : 'Arial','width' : '15%', 'margin': 'none', 'padding' : '10px' }),
+            "value": "itemovertotalspending"
+        },
+        {
+            'label':html.Div(['Item Over Quantity Purchased'], style={'color': 'Black', 'font-size': 15, 'display' : 'inline-block', 'font-family' : 'Arial','width' : '15%', 'margin': 'none', 'padding' : '10px' }),
+            "value": "itemoverquantity"
+        },
+        {
+            'label':html.Div(['NoOfSales Over Date'], style={'color': 'Black', 'font-size': 15, 'display' : 'inline-block', 'font-family' : 'Arial','width' : '15%', 'margin': 'none', 'padding' : '10px' }),
+            "value": "noofbuysoverdate"
+        }],  id='radio-button-picker')])
 
 def noofsalesoverdate():
     ax = 'Date'
-    ay = 'NoOfSales'
+    ay = 'NoOfItemsBought'
     dates=set([row['Date'] for row in data])
     header=(ax,ay)
     #subject to change based on the two values, currently date has to be x so we cannot just switch them over to fix .to_datetime() method
     newdata = {
-        ay: [],
-        ax: []
+        ay: [0],
+        ax: ['14.08.23']
     }
     df = pandas.DataFrame(newdata)
     for date in dates:
@@ -35,23 +47,63 @@ def noofsalesoverdate():
                 sub_list.append(row)
         newrow = [str(len(sub_list)),date]
         df.loc[len(df)] = newrow
-# order the dates
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.sort_values('Date', ascending=True)
     return ax, ay,df
 
+def itemoverquantity():
+    ax = 'Item'
+    ay = 'Quantity'
+    #subject to change based on the two values, currently item is x so its easy
+    newdata = {
+        ax: [],
+        ay: []
+    }
+    df = pandas.DataFrame(newdata)
+    for row in data:
+         newrow=row[ax],row[ay]
+         df.loc[len(df)+1] = newrow
+    df = df.sort_values('Quantity', ascending=False)
+    return ax, ay,df
 
+def itemovertotalprice():
+    ax = 'Name'
+    ay = 'Total Spending'
+    names=set([row['Name'] for row in data])
+    header=(ax,ay)
+    #subject to change based on the two values, currently date has to be x so we cannot just switch them over to fix .to_datetime() method
+    newdata = {
+        ay: [],
+        ax: []
+    }
+    df = pandas.DataFrame(newdata)
+    for name in names:
+        sub_value=0
+        for row in data:
+            if row['Name']==name:
+                sub_list = sub_list+row['Price']
+        newrow = [name,sub_value]
+        df.loc[len(df)] = newrow
+    return ax, ay,df
 
-        
 
 @app.callback(
     Output('graph', 'figure'),
-    Input('button', 'value'))
+    Input('radio-button-picker', 'value'))
 def display_graph(value):
-    datandaxis = noofsalesoverdate()
-    fig = px.line(datandaxis[2], x=datandaxis[0], y=
-                  datandaxis[1])
-    return fig
+    if value=='itemoverquantity':
+        datandaxis = itemoverquantity()
+        fig = px.bar(datandaxis[2], x=datandaxis[0], y=
+                datandaxis[1], width=2400, height=1000)
+    elif value=='noofbuysoverdate':
+        datandaxis = noofsalesoverdate()
+        fig = px.line(datandaxis[2], x=datandaxis[0], y=
+                datandaxis[1],  width=2400, height=1000)
+        
+    elif value=='itemovertotalspending':
+        datandaxis = itemovertotalprice()
+        fig = px.line(datandaxis[2], x=datandaxis[0], y=
+                datandaxis[1],  width=2400, height=1000)
+    
 
-
-app.run_server(debug=True)
+app.run_server()
